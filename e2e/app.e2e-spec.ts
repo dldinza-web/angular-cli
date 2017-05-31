9bd8dcf08670ec4f44bea9d39f74b652343ba904
Angular CLI <angular-cli@angular.io>
2017-05-31 14:47:25 -0400

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { MejorveFrontendPage } from './app.po';
+
+describe('mejorve-frontend App', () => {
+  let page: MejorveFrontendPage;
+
+  beforeEach(() => {
+    page = new MejorveFrontendPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
