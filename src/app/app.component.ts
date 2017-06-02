f9ffabb1136712336626e299ccb15903bc8ff5c4
Dennys Lopez Dinza <dldinza.web@gmail.com>
2017-06-02 15:43:31 -0400

    - Load global js files and instance them

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as $ from 'jquery';
+import 'dataTables';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements AfterViewInit {
   title = 'MEJORVE!';
+
+  ngAfterViewInit() {
+    $('table').dataTable();
+  }
 }
